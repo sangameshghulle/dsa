11adaf69d8ab3bf7b1b4a1bf40c6737dd88f25fe
sangameshghulle <sangameshghulle@gmail.com>
2026-09-20 08:51:32 +0530

    Time: 7 ms (90.38%), Space: 20.6 MB (40.38%) - LeetHub

---
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
@@ -0,0 +1,17 @@
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        if not s:
+            return 0
+        ans=0
+        stack=[-1]
+        
+        for i,ch in enumerate(s):
+            if ch=='(':
+                stack.append(i)
+            else:
+                stack.pop()
+                if not stack:
+                    stack.append(i)
+                else:
+                    ans=max(ans,i-stack[-1])
+        return ans
