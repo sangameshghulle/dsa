f7f845a18dbb23cedf855181ca84f9a53e92109c
sangameshghulle <sangameshghulle@gmail.com>
2026-09-20 09:26:12 +0530

    Time: 43 ms (51.94%), Space: 20.3 MB (96.34%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        stack=[]
+        invalid=set()
+        for i,ch in enumerate(s):
+            if ch==')':
+                if not stack:
+                    invalid.add(i)
+                else:
+                    stack.pop()
+                
+            elif ch=='(':
+                stack.append(i)
+        
+        while stack:
+            invalid.add(stack.pop())
+        
+        ans=''
+        for i,ch in enumerate(s):
+            if i not in invalid:
+                ans+=ch
+        
+        return ans
