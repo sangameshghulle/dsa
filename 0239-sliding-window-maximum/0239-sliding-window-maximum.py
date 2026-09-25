5f9a7ebf0d35e97b93bcae06065da65ab4d0a3f3
sangameshghulle <sangameshghulle@gmail.com>
2026-09-25 08:51:09 +0530

    Time: 211 ms (25.37%), Space: 35.5 MB (22.02%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -6,21 +6,17 @@ class Solution:
         result = []
 
         for i in range(len(nums)):
-
-            # 1. Remove indices outside the current window
-            while q and q[0] < i - k + 1:
+            
+            while q and q[0]<i-k+1:
                 q.popleft()
-
-            # 2. Remove smaller/equal values from the back
-            #    They can never become the maximum again.
-            while q and nums[q[-1]] <= nums[i]:
+            
+            while q and nums[q[-1]]<=nums[i]:
                 q.pop()
 
-            # 3. Add current index
             q.append(i)
 
-            # 4. Window is complete -> front is maximum
-            if i >= k - 1:
+
+            if i>=k-1:
                 result.append(nums[q[0]])
 
         return result
